7fd93297ba12c99ad79421a8cdf6664f4de5ee2c
imglmd <dangdatmini@gmail.com>
2025-11-11 18:08:36 +0300

    arch

--- app/src/main/java/com/kiryha/noting/presentation/screens/auth/AuthState.kt
+++ app/src/main/java/com/kiryha/noting/presentation/screens/auth/AuthState.kt
@@ -1,9 +1,9 @@
 package com.kiryha.noting.presentation.screens.auth
 
 sealed class AuthState {
-    object Initial : AuthState()
-    object Loading : AuthState()
-    object Authenticated : AuthState()
-    object Unauthenticated : AuthState()
+    data object Initial : AuthState()
+    data object Loading : AuthState()
+    data object Authenticated : AuthState()
+    data object Unauthenticated : AuthState()
     data class Error(val message: String) : AuthState()
 }
